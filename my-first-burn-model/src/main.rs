aef1c143c5ac408ec4275d810cbdcf95d3413375
TLightSky <wangchao719@gmail.com>
2024-05-09 22:48:05 +0800

    add main.rs

--- my-first-burn-model/src/main.rs
+++ my-first-burn-model/src/main.rs
@@ -1,8 +1,21 @@
+use burn::backend::{Autodiff, Wgpu};
+use burn::backend::wgpu::AutoGraphicsApi;
+use burn::optim::AdamConfig;
+use crate::model::ModelConfig;
+
 mod model;
 mod data;
 mod training;
 
 
 fn main() {
-    println!("Hello, world!");
+    type MyBackend = Wgpu<AutoGraphicsApi, f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyBackend>;
+
+    let device = burn::backend::wgpu::WgpuDevice::default();
+    crate::training::train::<MyAutodiffBackend>(
+        "/tmp/guide",
+        crate::training::TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+        device,
+    );
 }
